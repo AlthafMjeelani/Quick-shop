c31674c28cb75bbd7f426847f2efd9149d634f2d
althafm2002 <althafjeelani159@gmail.com>
2022-10-24 15:33:33 +0530

    add api integeration for signUp page used verify otp

--- lib/screens/authentication/service/signup/verify_otp_service.dart
+++ lib/screens/authentication/service/signup/verify_otp_service.dart
@@ -5,12 +5,14 @@ import 'package:ecommerse/screens/bottomnavigation/view/bottom_navigation.dart';
 import 'package:ecommerse/utils/app_popups.dart';
 import 'package:ecommerse/utils/exception/dio_exception.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class VerifyotpService {
   //------------------- Verify Otp ----------------------
 
   static Future<void> signUpVerifyOtp(
       UserVerifyOtpModel model, context, bool isLoading) async {
+    const storage = FlutterSecureStorage();
     try {
       isLoading = true;
       final response = await Dio().post(
@@ -19,6 +21,8 @@ class VerifyotpService {
       );
       if (response.statusCode! >= 200 && response.statusCode! <= 299) {
         log(response.data.toString());
+
+        storage.write(key: 'token', value: response.data);
         AppPopUps.showToast('Created Successfully', Colors.green);
         isLoading = false;
         Navigator.of(context).pushAndRemoveUntil(
